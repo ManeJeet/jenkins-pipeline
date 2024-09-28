f320caf408a576a08ed989844c080f623616398e
Jeet Manoj Mane <141498735+ManeJeet@users.noreply.github.com>
2024-09-28 18:44:55 +1000

    Update Jenkinsfile1.groovy

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -5,6 +5,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building the code using Maven'
+                echo 'maven build'
             }
         }
         stage('Unit and Integration Tests') {
